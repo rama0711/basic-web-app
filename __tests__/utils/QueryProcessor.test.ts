fd626071d0d477c82bdfb34b1451082633c95840
rama0711 <rsulaima@andrew.cmu.edu>
2023-10-01 12:25:34 +0300

    added new test for the andrew query

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,10 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+    test('should return andrew id', () => {
+        const query = "what is your andrew id?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "My Andrew ID is rsulaima."));
+    });
 });
